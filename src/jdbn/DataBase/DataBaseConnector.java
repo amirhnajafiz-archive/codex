d2bb1225aad72b381a69e8bc41ca3d4214c05b7c
9831065 <najafizadeh21@gmail.com>
2021-05-31 14:41:54 +0430

    Insert notes into database setup

--- src/jdbn/DataBase/DataBaseConnector.java
+++ src/jdbn/DataBase/DataBaseConnector.java
@@ -1,6 +1,8 @@
 package jdbn.DataBase;
 
 import java.sql.*;
+import java.util.ArrayList;
+import jdbn.Model.Note;
 
 public class DataBaseConnector implements Runnable {
     private Connection connection;
@@ -13,10 +15,45 @@ public class DataBaseConnector implements Runnable {
         this.killProcess = true;
     }
 
+    public String saveNotes(ArrayList<Note> notes, String email)
+    {
+        String query = "insert into NP_Notes values(?, ?, ?, ?);";
+        PreparedStatement statement = null;
+
+        try {
+            statement = connection.prepareStatement(query);
+            for (Note note : notes)
+            {
+                statement.setString(1, email);
+                statement.setString(2, note.getHeader());
+                statement.setString(3, note.getContent());
+                statement.setDate(4, (Date) note.getDate());
+                statement.addBatch();
+            }
+
+            int[] batchRowsAffected = statement.executeBatch();
+
+            System.out.println("> Database updated : successfully");
+            return "Complete";
+        } catch (SQLException exception) {
+            System.out.println("> Database error : " + exception.getMessage());
+        } finally {
+            if (statement != null)
+            {
+                try {
+                    statement.close();
+                } catch (SQLException ex) {
+                    System.out.println("Disconnection error : " + ex.getMessage());
+                }
+            }
+        }
+        return "System failed";
+    }
+
     public String userLogIn(String email)
     {
         Statement statement = null;
-        String query = "select * from NP_Users where user_mail = " + email;
+        String query = "select * from NP_Users where user_mail = " + email + ";";
         try {
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
@@ -24,7 +61,7 @@ public class DataBaseConnector implements Runnable {
             {
                 return "User Exists";
             } else {
-                query = "insert into NP_Users(user_mail) values (" + email + ")";
+                query = "insert into NP_Users(user_mail) values (" + email + ");";
                 int rowsAffected = statement.executeUpdate(query);
                 return "User Created";
             }
