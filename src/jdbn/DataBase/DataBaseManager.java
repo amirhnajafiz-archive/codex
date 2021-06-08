3ecd0f2ad5a698c614d1a22a23b0377f9530d4c1
9831065 <najafizadeh21@gmail.com>
2021-06-08 13:28:20 +0430

    Setup database information shower

--- src/jdbn/DataBase/DataBaseManager.java
+++ src/jdbn/DataBase/DataBaseManager.java
@@ -7,6 +7,9 @@ public class DataBaseManager
 	public static String mainTable = "NP_Users";
 	public static String dataTable = "NP_Notes";
 
+	private static String users_query = "select * from '" + DataBaseManager.mainTable + "'";
+	private static String data_query = "select * from '" + DataBaseManager.dataTable + "'";
+
 	public static void initializeTables(Connection connection)
 	{
 		try {
@@ -35,6 +38,7 @@ public class DataBaseManager
 	    try {
 	        String URL = "jdbc:sqlite:F:\\Desktop\\JDBC\\chinook.db";
 	        connection = DriverManager.getConnection(URL);
+
 	        // Get information about tables in our database
 			DatabaseMetaData metaData = connection.getMetaData();
 			ResultSet resultSetting = metaData.getTables(null, null, null, new String[]{"TABLE"});
@@ -45,6 +49,34 @@ public class DataBaseManager
 				System.out.println(resultSetting.getString("TABLE_NAME"));
 			}
 			System.out.println("------");
+			System.out.println("------");
+
+			// Get information about users
+			Statement statement = connection.createStatement();
+			ResultSet users = statement.executeQuery(DataBaseManager.users_query);
+			ResultSet notes = statement.executeQuery(DataBaseManager.data_query);
+
+			System.out.println("Printing \"Users\"");
+			System.out.println("----------------------------------");
+			while (users.next())
+			{
+				System.out.println("User: " + users.getString(1));
+			}
+			System.out.println("------");
+			System.out.println("------");
+
+			System.out.println("Printing \"Notes\"");
+			System.out.println("----------------------------------");
+			while (notes.next())
+			{
+				System.out.print("User: " + notes.getString(1) + "| ");
+				System.out.print("Title: " + notes.getString(2) + "\n");
+				System.out.print("Content:\n" + notes.getString(3) + "\n");
+				System.out.print("Last modified: " + notes.getDate(4) + "\n");
+				System.out.println("---");
+			}
+			System.out.println("------");
+
 			// Creating my table
 			initializeTables(connection);
         } catch (SQLException e) {
